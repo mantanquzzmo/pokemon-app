7052e97d3f98f566ad2a2333c6fdc7b352cfcbf0
mantanquzzmo <quzzmo@hotmail.com>
2020-01-03 11:32:28 +0100

    sets up connection to api and useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React, { useState } from 'react';
-import PokemonList from './PokemonList'
+import React, { useState, useEffect } from 'react';
+import PokemonList from './PokemonList';
+import axios from 'axios'
 
 function App() {
-  const [pokemon, setPokemon] = useState(["bulbasaur", "charmander"])
+  const [pokemon, setPokemon] = useState([])
+
+  useEffect(() => {
+    axios.get("https://pokeapi.co/api/v2/pokemon").then(res => {
+      setPokemon(res.data.results.map(p => p.name))
+    })
+  }, [])
+
+  axios.get("https://pokeapi.co/api/v2/pokemon").then(res => {
+    setPokemon(res.data.results.map(p => p.name))
+  })
+
   return (
     <PokemonList pokemon={pokemon} />
   );
