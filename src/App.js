b635e0b4bd8fbb50ba72e66077a65101e3dde703
mantanquzzmo <quzzmo@hotmail.com>
2020-01-03 20:34:01 +0100

    adds loading and next, prevpage from api

--- src/App.js
+++ src/App.js
@@ -4,16 +4,22 @@ import axios from 'axios'
 
 function App() {
   const [pokemon, setPokemon] = useState([])
+  const [currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon")
+  const [nextPageUrl, setNextPageUrl] = useState()
+  const [prevPageUrl, setPrevPageUrl] = useState()
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    axios.get("https://pokeapi.co/api/v2/pokemon").then(res => {
+    setLoading(true)
+    axios.get(currentPageUrl).then(res => {
+      setLoading(false)
+      setNextPageUrl(res.data.next)
+      setPrevPageUrl(res.data.previous)
       setPokemon(res.data.results.map(p => p.name))
     })
-  }, [])
+  }, [currentPageUrl])
 
-  axios.get("https://pokeapi.co/api/v2/pokemon").then(res => {
-    setPokemon(res.data.results.map(p => p.name))
-  })
+  if (loading) return "Loading..."
 
   return (
     <PokemonList pokemon={pokemon} />
